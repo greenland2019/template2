b707103840cf60cc9724813fd5e541bca7fef41f
clydemino <clydevianney.omogntap@esprit.tn>
2019-02-16 18:41:55 +0100

    second_commit

--- src/PlantsBundle/Controller/DefaultController.php
+++ src/PlantsBundle/Controller/DefaultController.php
@@ -10,21 +10,41 @@ use PlantsBundle\Entity\Personne;
 
 class DefaultController extends Controller
 {
+    /**
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse|Response
+     */
     public function indexAction(Request $request)
     {
         $em = $this->getDoctrine()->getManager();
-        if ($request->get("choix")=="connexion") {
-            $user = $em->getRepository('PlantsBundle:Personne')->findOneBy(['email' => $request->get("email"), 'password' => $request->get("password")]);
-            if ($user) {
-                $sesion = $request->getSession();
-                $sesion->set('login', $user->getPrenom());
-                return $this->redirectToRoute('plants_homepage');
+        if ($request->isXmlHttpRequest()) {
 
-            }
+                $user = $em->getRepository('PlantsBundle:Personne')->findOneBy(['email' => $request->get("email"), 'password' => $request->get("password")]);
+                if ($user) {
+                    $sesion = $request->getSession();
+                    $sesion->set('login', $user->getPrenom());
+
+                    $response = new Response(json_encode(array(
+                        'result' => 'success',
+                        'login' =>  $user->getPrenom()
+                    )));
+                    $response->headers->set('Content-Type', 'application/json');
+
+                    return $response;
+                }
+
+            $response = new Response(json_encode(array(
+                'result' => 'fail'
+
+            )));
+            $response->headers->set('Content-Type', 'application/json');
+
+            return $response;
+
+                //$this->addFlash("error", "Informations erronees");
+                //die('Informations erronees');
 
 
-            $this->addFlash("error", "Informations erronees");
-            return $this->render('@Plants/template/index.html.twig');
         }
         return $this->render('@Plants/template/index.html.twig');
     }
